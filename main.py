f15fdda06592ceda2c1b400799331fd00fc79421
Илья Афанасьев <iafanasevmail@gmail.com>
2024-05-21 22:55:01 +0300

    update play in main

--- main.py
+++ main.py
@@ -12,9 +12,18 @@ def display_hangman(tries):
 
 
 def play():
-    word = get_word()
-
-    # print(display_hangman(tries))
+    print('Давайте играть в угадайку слов!')
+    while True:
+        word = get_word()
+        word_completion = ['_' for _ in range(len(word))]
+        guessed = False
+        guessed_letters = []
+        guessed_words = []
+        tries = 6
+        while not guessed and tries > 0:
+            print(display_hangman(tries))
+            print(*word_completion)
+            data = input('Введите символ или слово целиком\n').upper()
 
 
 if __name__ == '__main__':
