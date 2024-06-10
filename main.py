1265a230a704650bc12760ea577ed581e3f512be
Илья Афанасьев <iafanasevmail@gmail.com>
2024-06-10 22:16:19 +0300

    added exception handling

--- main.py
+++ main.py
@@ -32,54 +32,57 @@ def is_valid_input(data: str) -> bool:
 def play():
     print('Давайте играть в угадайку слов!')
     while True:
-        word = get_word()
-        word_completion = ['_' for _ in range(len(word))]
-        guessed = False
-        guessed_letters = []
-        guessed_words = []
-        tries = 6
-        while not guessed and tries > 0:
-            print(display_hangman(tries))
-            print(*word_completion)
-            data = input('Введите символ или слово целиком\n').upper()
-            if not is_valid_input(data):
-                continue
-            if len(data) == 1:
-                count = 0
-                if is_repeat(data, guessed_letters, guessed_words):
-                    print('Вы уже вводили эту букву!')
+        try:
+            word = get_word()
+            word_completion = ['_' for _ in range(len(word))]
+            guessed = False
+            guessed_letters = []
+            guessed_words = []
+            tries = 6
+            while not guessed and tries > 0:
+                print(display_hangman(tries))
+                print(*word_completion)
+                data = input('Введите символ или слово целиком\n').upper()
+                if not is_valid_input(data):
                     continue
-                guessed_letters.append(data)
-                for cur in range(len(word)):
-                    if word[cur] == data:
-                        word_completion[cur] = data
-                        count += 1
-                if count == 0:
-                    print('Неверно!')
-                    tries -= 1
-                elif ''.join(word_completion) == word:
-                    print('Поздравляем, вы угадали слово! Вы победили!')
-                    guessed = True
+                if len(data) == 1:
+                    count = 0
+                    if is_repeat(data, guessed_letters, guessed_words):
+                        print('Вы уже вводили эту букву!')
+                        continue
+                    guessed_letters.append(data)
+                    for cur in range(len(word)):
+                        if word[cur] == data:
+                            word_completion[cur] = data
+                            count += 1
+                    if count == 0:
+                        print('Неверно!')
+                        tries -= 1
+                    elif ''.join(word_completion) == word:
+                        print('Поздравляем, вы угадали слово! Вы победили!')
+                        guessed = True
+                    else:
+                        print('Поздравляем, вы угадали букву!')
                 else:
-                    print('Поздравляем, вы угадали букву!')
-            else:
-                if is_repeat(data, guessed_letters, guessed_words):
-                    print('Вы уже вводили это слово!')
-                    continue
-                guessed_words.append(data)
-                if data == word:
-                    print('Поздравляем, вы угадали слово! Вы победили!')
-                    guessed = True
-                else:
-                    print('Неверно!')
-                    tries -= 1
-        if not guessed:
-            print(display_hangman(tries))
-            print('Вы проиграли!')
-            print(f'Правильный ответ {word}')
-        answer = input('Хотите сыграть ещё?\n 1 - да, 2 - нет\n')
-        if answer == '2':
-            break
+                    if is_repeat(data, guessed_letters, guessed_words):
+                        print('Вы уже вводили это слово!')
+                        continue
+                    guessed_words.append(data)
+                    if data == word:
+                        print('Поздравляем, вы угадали слово! Вы победили!')
+                        guessed = True
+                    else:
+                        print('Неверно!')
+                        tries -= 1
+            if not guessed:
+                print(display_hangman(tries))
+                print('Вы проиграли!')
+                print(f'Правильный ответ {word}')
+            answer = input('Хотите сыграть ещё?\n 1 - да, 2 - нет\n')
+            if answer == '2':
+                break
+        except Exception as e:
+            print(f'Произошла ошибка: {e}')
     print('Спасибо, что играли в нашу игру! До встречи!')
 
 
