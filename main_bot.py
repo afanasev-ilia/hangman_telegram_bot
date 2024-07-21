b644e6eef447010a5a1e1c575d644be903dfed7a
Илья Афанасьев <iafanasevmail@gmail.com>
2024-07-21 19:46:24 +0300

    added start function

--- main_bot.py
+++ main_bot.py
@@ -1,17 +1,14 @@
 import os
 import logging
-# import requests
+
 
 from dotenv import load_dotenv
 from pathlib import Path
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram import ReplyKeyboardMarkup, Update
 from telegram.ext import (
     CommandHandler,
     CallbackContext,
-    ConversationHandler,
-    MessageHandler,
     Updater,
-    Filters,
 )
 
 load_dotenv()
@@ -28,19 +25,6 @@ logging.basicConfig(
     ),
 )
 
-# WORK_EMPLOYEE, ORDER, ITEM_ORDER, EXECUTION_TIME, WORK_IMAGE = (
-#     'employee',
-#     'order',
-#     'item_order',
-#     'execution_time',
-#     'image',
-# )
-
-# CLEAN_EMPLOYEE, CLEAN_IMAGE = (
-#     'employee',
-#     'image',
-# )
-
 
 def start(update: Update, context: CallbackContext) -> int:
     button = ReplyKeyboardMarkup(
@@ -52,110 +36,6 @@ def start(update: Update, context: CallbackContext) -> int:
         text='Здравствуйте!',
         reply_markup=button,
     )
-
-
-# def work_report(
-#     update: Update,
-#     context: CallbackContext,
-# ) -> int:
-#     context.user_data[WORK_EMPLOYEE] = Employee.objects.get(
-#         external_id=update.effective_chat.id
-#     ).id
-#     update.message.reply_text(
-#         'Введите номер счета что бы продолжить',
-#         reply_markup=ReplyKeyboardRemove(),
-#     )
-#     return ORDER
-
-
-# def order_handler(
-#         update: Update,
-#         context: CallbackContext
-# ) -> int:
-#     context.user_data[ORDER] = int(update.message.text)
-#     update.message.reply_text(
-#         'Введите порядковый номер позиции из счета',
-#     )
-#     return ITEM_ORDER
-
-
-# def item_handler(
-#         update: Update,
-#         context: CallbackContext
-# ) -> int:
-#     context.user_data[ITEM_ORDER] = int(update.message.text)
-#     update.message.reply_text('Укажите время выполнения в минутах')
-#     return EXECUTION_TIME
-
-
-# def time_handler(
-#         update: Update,
-#         context: CallbackContext
-# ) -> int:
-#     context.user_data[EXECUTION_TIME] = int(update.message.text)
-#     update.message.reply_text('Приложите фотографию')
-#     return WORK_IMAGE
-
-
-# def cancel_handler(
-#         update: Update,
-#         context: CallbackContext
-# ) -> int:
-#     update.message.reply_text('Отчет не отправлен')
-#     return ConversationHandler.END
-
-
-# def clean_report(
-#         update: Update,
-#         context: CallbackContext,
-# ) -> int:
-#     context.user_data[CLEAN_EMPLOYEE] = Employee.objects.get(
-#         external_id=update.effective_chat.id
-#     ).id
-#     update.message.reply_text(
-#         'Приложите фотографию',
-#         reply_markup=ReplyKeyboardRemove(),
-#     )
-#     return CLEAN_IMAGE
-
-
-# work_report_handler = ConversationHandler(
-#     entry_points=[
-#         MessageHandler(
-#             Filters.text('Отчет о проделанной работе'),
-#             work_report,
-#         ),
-#     ],
-#     states={
-#         ORDER: [
-#             MessageHandler(
-#                 Filters.all,
-#                 order_handler,
-#             ),
-#         ],
-#         ITEM_ORDER: [
-#             MessageHandler(
-#                 Filters.all,
-#                 item_handler,
-#             ),
-#         ],
-#         EXECUTION_TIME: [
-#             MessageHandler(
-#                 Filters.all,
-#                 time_handler,
-#             ),
-#         ],
-#         WORK_IMAGE: [
-#             MessageHandler(
-#                 Filters.all,
-#                 work_image_handler,
-#             ),
-#         ],
-#     },
-#     fallbacks=[
-#         CommandHandler('cancel', cancel_handler),
-#     ],
-# )
 
 
 updater = (
