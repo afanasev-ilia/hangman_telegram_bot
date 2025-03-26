a77b325dd8e7b3299b72f488486ce3ae56b97464
Илья Афанасьев <iafanasevmail@gmail.com>
2025-03-26 21:21:18 +0300

    changed get_word function

--- hangman_utils.py
+++ hangman_utils.py
@@ -3,8 +3,23 @@ import random
 from hangman_data import stages, word_list
 
 
-def get_word():
-    return random.choice(word_list).upper()
+def get_word_by_length(min_length, max_length):
+    filtered_words = [word for word in word_list if min_length <= len(word) <= max_length]
+    return random.choice(filtered_words).upper()
+
+
+def get_word(difficulty):
+    if difficulty == 'easy':
+        return get_word_by_length(3, 5)
+    elif difficulty == 'medium':
+        return get_word_by_length(6, 8)
+    elif difficulty == 'hard':
+        return get_word_by_length(9, 16)
+    else:
+        return random.choice(word_list).upper()
+    
+# def get_word():
+#     return random.choice(word_list).upper()
 
 
 def display_hangman(tries):
